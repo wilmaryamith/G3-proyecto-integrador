356f722313a5e3fda88a6028078b7ce1737da2ae
Julian-1120 <jmacias7@udi.edu.co>
2024-11-15 19:25:57 -0500

    Nuevas metodos para mejor control de los datos de las clases

---
+++ src/Interface/IGestorConexion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interface;
+
+/**
+ *
+ * @author julia
+ */
+public interface IGestorConexion {
+    void conectar();
+    void desconectar();
+    boolean testearConexion();
+}
